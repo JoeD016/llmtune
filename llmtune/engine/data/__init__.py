30cd5937dd2d838fcd6f12e725e69dde801d7d9a
JoeD016 <joemeliodasdong@gmail.com>
2023-06-10 18:19:28 -0500

    add export of `TrainSAMsum`

--- llmtune/engine/data/__init__.py
+++ llmtune/engine/data/__init__.py
@@ -1,3 +1,4 @@
 from llmtune.engine.data.text import TrainTxt
 from llmtune.engine.data.alpaca import TrainSAD
 from llmtune.engine.data.gpt4all import TrainGPT4All
+from llmtune.engine.data.samsum import TrainSAMsum
