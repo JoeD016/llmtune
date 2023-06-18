aea3e1ba9cf56812124b4d3744dc6e9894f8b1cb
JoeD016 <joemeliodasdong@gmail.com>
2023-06-17 23:58:48 -0500

    try fix eval -3

--- llmtune/run.py
+++ llmtune/run.py
@@ -189,43 +189,121 @@ def evaluate_metrics(args):
     from datasets import load_dataset 
     import llmtune.executor as llmtune
     from llmtune.engine.data.samsum import make_output, InstructDataset
+    from transformers import AutoTokenizer
 
 
     # Metric
     metric = evaluate.load("rouge")
 
-
-    llm, tokenizer = llmtune.load_llm(args.model, args.weights)
-    llm.config.num_beams = 5
-    llm.config.max_length = 50
     
-    def evaluate_peft_model(llm,tokenizer,sample,args):
-        # Load dataset from the hub and get a sample
-        prompt = f"### Summarized this: {sample}\n ### Output: "
+    # def evaluate_peft_model(llm,tokenizer,sample,args):
+    #     # Load dataset from the hub and get a sample
+    #     prompt = f"### Summarized this: {sample}\n ### Output: "
         
-        ## Currently the hyper paramters are hard-coded.
-        output = llmtune.generate(
-        llm, 
-        tokenizer, 
-        prompt, 
-        10, 
-        520, 
-        args.temperature,        
-        args.top_k, 
-        0.9, 
+    #     ## Currently the hyper paramters are hard-coded.
+    #     output = llmtune.generate(
+    #     llm, 
+    #     tokenizer, 
+    #     prompt, 
+    #     10, 
+    #     520, 
+    #     args.temperature,        
+    #     args.top_k, 
+    #     0.9, 
+    #     )
+        
+    #     output = make_output(output)
+
+    #     # print(f"Output:\n{output}")
+    #     # Some simple post-processing
+    #     return output
+    
+    def fix_tokenizer(tokenizer):
+        # Fixing broken tokenizers
+        special_tokens = dict()
+        for token_id in range(1000):
+            token = tokenizer.convert_ids_to_tokens(token_id)
+            if tokenizer.pad_token_id in (None, tokenizer.vocab_size) and "pad" in token:
+                special_tokens["pad_token"] = token
+            if tokenizer.bos_token_id in (None, tokenizer.vocab_size) and "<s>" in token:
+                special_tokens["bos_token"] = token
+            if tokenizer.eos_token_id in (None, tokenizer.vocab_size) and "</s>" in token:
+                special_tokens["eos_token"] = token
+            if tokenizer.unk_token_id in (None, tokenizer.vocab_size) and "unk" in token:
+                special_tokens["unk_token"] = token
+            if tokenizer.sep_token_id in (None, tokenizer.vocab_size) and "sep" in token:
+                special_tokens["sep_token"] = token
+
+        if tokenizer.sep_token_id in (None, tokenizer.vocab_size) and "bos_token" in special_tokens:
+            special_tokens["sep_token"] = special_tokens["bos_token"]
+
+        if tokenizer.pad_token_id in (None, tokenizer.vocab_size) and "pad_token" not in special_tokens:
+            if tokenizer.unk_token_id is not None:
+                special_tokens["pad_token"] = tokenizer.unk_token
+            else:
+                special_tokens["pad_token"] = "<|pad|>"
+
+        if tokenizer.sep_token_id in (None, tokenizer.vocab_size) and "sep_token" not in special_tokens:
+            if tokenizer.bos_token_id is not None:
+                special_tokens["sep_token"] = tokenizer.bos_token
+            else:
+                special_tokens["sep_token"] = "<|sep|>"
+
+        tokenizer.add_special_tokens(special_tokens)
+
+        print("Vocab size: ", tokenizer.vocab_size)
+        print("PAD: ", tokenizer.pad_token_id, tokenizer.pad_token)
+        print("BOS: ", tokenizer.bos_token_id, tokenizer.bos_token)
+        print("EOS: ", tokenizer.eos_token_id, tokenizer.eos_token)
+        print("UNK: ", tokenizer.unk_token_id, tokenizer.unk_token)
+        print("SEP: ", tokenizer.sep_token_id, tokenizer.sep_token)
+        return tokenizer
+
+    def fix_model(model, tokenizer, use_resize=True):
+        model.config.pad_token_id = tokenizer.pad_token_id
+        assert model.config.pad_token_id is not None
+
+        bos_candidates = (
+            tokenizer.bos_token_id,
+            tokenizer.cls_token_id,
+            tokenizer.sep_token_id,
+            tokenizer.unk_token_id
         )
-        
-        output = make_output(output)
+        for bos_candidate in bos_candidates:
+            model.config.bos_token_id = bos_candidate
+            if bos_candidate is not None:
+                break
+        assert model.config.bos_token_id is not None
+        model.config.decoder_start_token_id = model.config.bos_token_id
 
-        # print(f"Output:\n{output}")
-        # Some simple post-processing
-        return output
+        eos_candidates = (tokenizer.eos_token_id, tokenizer.sep_token_id)
+        for eos_candidate in eos_candidates:
+            model.config.eos_token_id = eos_candidate
+            if eos_candidate is not None:
+                break
+        assert model.config.eos_token_id is not None
+
+        if use_resize:
+            model.resize_token_embeddings(len(tokenizer))
+
+        return model
     
-    def evaluate_sample(llm,tokenizer,sample,args):
+    model_name = "huggyllama/llama-13b"
+    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
+    tokenizer = fix_tokenizer(tokenizer)
+
+    llm, _ = llmtune.load_llm(args.model, args.weights)
+    model = fix_model(llm, tokenizer, use_resize=False)
+    model.config.num_beams = 5
+    model.config.max_length = 50
+
+    model = llmtune.load_adapter(model, adapter_path=args.adapter)
+
+    def evaluate_sample(model,tokenizer,sample):
         # Load dataset from the hub and get a sample
         sample_word = f"### Summarize this: {sample}\n ### Output: "
         input_ids = tokenizer(sample_word, return_tensors="pt", truncation=True).input_ids.cuda()
-        outputs = llm.generate(input_ids=input_ids, do_sample=True, top_p=0.9, max_new_tokens = 65)
+        outputs = model.generate(input_ids=input_ids, do_sample=True, top_p=0.9, max_new_tokens = 65)
         output = tokenizer.decode(outputs[0].detach().cpu().numpy(), skip_special_tokens=True).replace(sample_word,"")
         print(f"Output:\n{output}")
         # Some simple post-processing
@@ -238,7 +316,7 @@ def evaluate_metrics(args):
     # this can take ~45 minutes
     predictions = []
     for sample in tqdm(dataset['test']['dialogue'][0:args.test_count]): ## currently only support sequential sampling (from 0 to 200 for example)
-        p = evaluate_sample(llm,tokenizer,sample,args)
+        p = evaluate_sample(model,tokenizer,sample)
         predictions.append(p)
 
     # compute metric 
